f5da54affd4e6d1968afd6947e3c3bf613588b6f
wltnthss <60785586+wltnthss@users.noreply.github.com>
2021-01-24 12:39:55 +0900

    add LeapYear

--- src/sub1/calendar/Prompt.java
+++ src/sub1/calendar/Prompt.java
@@ -3,18 +3,20 @@ package sub1.calendar;
 import java.util.Scanner;
 
 public class Prompt {	
-	
-	private final static String PROMPT = "cal> ";
-	
+		
 	public void runPrompt() {
 		Scanner sc = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
 		int month = 1;
+		int year = 2021;
 		
 		while(true) {
+			System.out.println("년도를 입력하세요");
+			System.out.print("YEAR> ");
+			year = sc.nextInt();
 			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
+			System.out.print("MONTH> ");
 			month = sc.nextInt();
 			if (month == -1) {
 				break;
@@ -23,7 +25,7 @@ public class Prompt {
 				System.out.println("\n1~12월 사이를 입력해주세요.\n");
 				continue;
 			}
-			cal.printCalendar(2021, month);
+			cal.printCalendar(year, month);
 		}			
 		System.out.println("Bye ~");
 		sc.close();
